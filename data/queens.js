e160994afe75bebd71c9fd03789399078af22b57
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-09 19:18:49 -0500

    updated queens data

--- data/queens.js
+++ data/queens.js
@@ -2,25 +2,25 @@ const queens = [
     {
         id: 1,
         dragName: "Lydia B Kollins",
-        "entrance line": "Did someone say my name three times?",
+        entrance_line: "Did someone say my name three times?",
         ratings: [3],
     },
     {
         id: 2,
         dragName: "Kori King",
-        "entrance line": "Good morning kingdommm",
+        entrance_line: "Good morning kingdommm",
         ratings: [2],
     },
     {
         id: 3,
         dragName: "Jewels Sparkles",
-        "entrance line": "Don't worry, I'm not just a bitch - I'm THAT bitch.",
+        entrance_line: "Don't worry, I'm not just a bitch - I'm THAT bitch.",
         ratings: [1, 1, 1],
     },
     {
         id: 4,
         dragName: "Sam Star",
-        "entrance line": "Well, I heard y'all were looking for America's Next Drag Superstar",
+        entrance_line: "Well, I heard y'all were looking for America's Next Drag Superstar",
         ratings: [4],
     },
 ]
