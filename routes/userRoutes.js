f059322b6f9cbd77fb4fe574bbd76756e8a9fa32
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-10 12:10:10 -0500

    added a user post a rate route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const users = require('../data/users')
+const rates = require('../data/rates')
 
 router.use(express.json())
 
@@ -59,6 +60,27 @@ router
 router.get("/:id/ratings", (req, res) => {
   const user = users.find((u) => u.id == req.params.id)
   if (user) res.json(user.myRatings)
+  else next()
+})
+router.post("/:id/rating", (req, res) => {
+  const user = users.find((u) => u.id == req.params.id)
+  if (user) {
+    if (req.body.userId && req.body.ratedQueenId && req.body.rate) {
+      if (rates.find((r) => r.userId == user.id && r.ratedQueenId == req.body.ratedQueenId)) {
+        res.json({ error: "Already rated this queen. Rate another queen!" })
+        return
+      }
+      const rate = {
+        id: rates[rates.length - 1].id + 1,
+        userId: req.params.id,
+        ratedQueenId: req.body.ratedQueenId,
+        rate: req.body.rate,
+      }
+
+      rates.push(rate)
+      res.json(rates[rates.length - 1])
+    } else res.json({ error: "Insufficent Data" })
+  }
   else next()
 })
 
